186b70245e23c3abd1570e5ad464cb378a830eb3
cchiffot <calvinchiffot@lttd-consulting.com>
2024-04-09 17:48:28 +0200

    fix text positions + feat hover start

--- portfolio-business-card/src/components/HomePage.js
+++ portfolio-business-card/src/components/HomePage.js
@@ -1,18 +1,28 @@
 import "./HomePage.css";
 
 function HomePage() {
+
+	function handleMouseEnter(e) {
+		e.target.style.cursor = 'pointer';
+		e.target.style.background = 'red';
+	  }
+
+	  function handleMouseLeave(e) {
+		e.target.style.background = '';
+	  }
+
 	return (
 		<div class="HomePage">
-			<div class="Contact">Contact</div>
-			<div class="Portfolio">Portfolio</div>
+			<div class="Contact" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Contact</div>
+			<div class="Portfolio" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Portfolio</div>
 			<div class="Name">Calvin CHIFFOT</div>
 			<div class="Title">Développeur Full-Stack</div>
 			<div class="Others">
-				<span>À propos de moi</span>
+				<span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>À propos de moi</span>
 				{", "}
-				<span>Compétences techniques</span>
+				<span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Compétences techniques</span>
 				{", "}
-				<span>Expériences profesionnelles</span>
+				<span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Expériences professionnelles</span>
 			</div>
 		</div>
 	);
